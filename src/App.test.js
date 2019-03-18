5d067ae7e1ae819f04a0c2ad8bfb5978c89f0edd
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-03-17 22:04:49 -0300

    create 'renders without error' test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
+import { exec } from 'child_process';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-it('renders without crashing', () => {
+it('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+it('renders increment button', () => {
+
+});
+
+it('renders counter display', () => {
+
+});
+
+it('couter starts at 0', () => {
+
+});
+
+it('clicking button increments counter display', () => {
 
 });
