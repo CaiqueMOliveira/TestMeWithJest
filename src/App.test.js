1552e0d161bcfb4266231e991bbf0dee8c242a97
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-03-17 22:37:20 -0300

    refactoring find method and shallow wrapper

--- src/App.test.js
+++ src/App.test.js
@@ -2,25 +2,32 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
-import { exec } from 'child_process';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const createShallowWrapper = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
+
+const findWrapperByTestAttribute = (wrapper, testAttributeValue) => {
+  return wrapper.find(`[data-test="${testAttributeValue}"]`);
+};
+
 it('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = createShallowWrapper();
+  const appComponent = findWrapperByTestAttribute(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
 it('renders increment button', () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='increment-button']");
+  const wrapper = createShallowWrapper();
+  const counterDisplay = findWrapperByTestAttribute(wrapper, "increment-button");
   expect(counterDisplay.length).toBe(1);
 });
 
 it('renders counter display', () => {
-  const wrapper = shallow(<App />);
-  const incrementButton = wrapper.find("[data-test='counter-display']");
+  const wrapper = createShallowWrapper();
+  const incrementButton = findWrapperByTestAttribute(wrapper, "counter-display");
   expect(incrementButton.length).toBe(1);
 });
 
